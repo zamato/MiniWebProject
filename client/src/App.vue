d5c7a7feaf543b9d75b3b8377c1ab0a2defdbad2
BIGKUNG-NOTEBOO\Niare <niare4731@gmail.com>
2018-07-13 00:08:49 +0700

    first commit project

---
+++ client/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+export default {
+  name: 'App'
+}
+</script>
---
+++ client/src/components/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id='map'></div>
+</template>
+
+<script>
+//import 'mapbox-gl/dist/mapbox-gl.css'
+//import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+
+export default {
+  mounted() {
+    console.log('mounted');
+    /*mapboxgl.accessToken = "<your access token here>";
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });*/
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
+
+

159658e2142bf46b5b92ecd71b3a3546943ff780
BIGKUNG-NOTEBOO\Niare <niare4731@gmail.com>
2018-07-13 23:06:11 +0700

    update

---
+++ api_server/index.py
@@ -0,0 +1,13 @@
+import time
+
+import redis
+from flask import Flask
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+    
+if __name__ == "__main__":
+    app.run()

d0589d50809ce549fd1c181f577249dac2ab7e96
BIGKUNG-NOTEBOO\Niare <niare4731@gmail.com>
2018-07-19 23:32:36 +0700

    update render map

--- web/src/components/index.vue
+++ web/src/components/index.vue
@@ -1,35 +1,57 @@
 <template>
-  <div id='map'>12354677777777</div>
+    <l-map :zoom="zoom" :center="center">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        <l-marker :lat-lng="marker"></l-marker>
+    </l-map>
 </template>
 
 <script>
-//import 'mapbox-gl/dist/mapbox-gl.css'
-//import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+//=====================Fix Marker=======================================
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+})
+//========================================================================
+
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
 
 export default {
-  mounted() {
-    console.log('mounted');
-    /*mapboxgl.accessToken = "<your access token here>";
-    var map = new mapboxgl.Map({
-      container: "map", // container id
-      style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-    });*/
-  }
+    data(){
+        return {
+            zoom:15,
+            center: L.latLng(0, 0),
+            url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            marker: L.latLng(0, 0)
+        }
+    },
+    components: { 
+        LMap,
+        LTileLayer,
+        LMarker
+    },
+    methods:{
+        initCurrentPosition(position){
+            this.center = L.latLng(position.coords.latitude, position.coords.longitude);
+            this.marker = L.latLng(position.coords.latitude, position.coords.longitude);
+        }
+    },
+    created(){
+        // One-shot position request.
+        navigator.geolocation.getCurrentPosition(this.initCurrentPosition);
+    }
 };
 </script>
 
 <style>
-body {
+html, body, #app {
+  height: 100%;
   margin: 0;
-  padding: 0;
-}
-#map {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 100%;
 }
 </style>
 
